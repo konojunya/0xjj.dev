81467145d65be8707afef11de789becf81290b03
Junya Kono <junya03dance@gmail.com>
2023-06-16 20:43:54 +0900

    feat: init

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "0xjj.dev",
+  description: "All of me",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
