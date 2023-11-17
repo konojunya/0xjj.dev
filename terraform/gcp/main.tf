5db0c9c00c2bd7e7eced91183abb79843ec8c9ed
JJ <junya03dance@gmail.com>
2023-11-18 03:09:56 +0900

    v2のインフラ作成 (#59)

---
+++ terraform/gcp/main.tf
@@ -0,0 +1,77 @@
+provider "google" {
+  project = var.project_id
+  region  = var.default_region
+}
+
+## IAM
+### GitHub Actions Service Account(eg. Cloud Run deploy from GitHub Actions)
+resource "google_service_account" "github_actions" {
+  account_id   = "github-actions"
+  display_name = "GitHub Actions Service Account"
+  description  = "Service Account for GitHub Actions"
+}
+
+resource "google_cloud_run_v2_service_iam_member" "github_actions" {
+  name     = google_cloud_run_v2_service.portfolio.name
+  location = var.default_region
+  project  = var.project_id
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "google_service_account_iam_member" "github_actions" {
+  service_account_id = google_service_account.github_actions.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/attribute.repository/konojunya/0xjj.dev"
+}
+
+## Cloud Run
+resource "google_cloud_run_v2_service" "portfolio" {
+  name     = "portfolio"
+  location = var.default_region
+
+  template {
+    containers {
+      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/portfolio/web"
+    }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 2
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "noauth" {
+  location = google_cloud_run_v2_service.portfolio.location
+  name     = google_cloud_run_v2_service.portfolio.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+## Workload Identity
+resource "google_iam_workload_identity_pool" "github_actions_pool" {
+  project                   = var.project_id
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "github-actions-pool"
+  description               = "Pool for GitHub Actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_actions" {
+  project                            = var.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+  display_name                       = "github-actions"
+  description                        = "GitHub Actions"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
---
+++ terraform/gcp/variable.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "project id"
+  type        = string
+  default     = "project_id"
+}
+
+variable "default_region" {
+  description = "default region"
+  type        = string
+  default     = "asia-northeast1"
+}
