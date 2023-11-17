93bccb6fbff7957840d2beb30e8757daa39746d7
konojunya <junya03dance@gmail.com>
2023-11-18 01:39:28 +0900

    update: application hello world

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "JJ",
+  description: "All of me",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
