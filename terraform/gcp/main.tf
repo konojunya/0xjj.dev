a994ed3a05a8fb69190f33d17d3fba7c4c6bbe11
konojunya <junya03dance@gmail.com>
2023-11-18 18:27:05 +0900

    fix

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -23,21 +23,21 @@ resource "google_artifact_registry_repository_iam_member" "github_actions" {
   project    = var.project_id
   location   = var.default_region
   repository = google_artifact_registry_repository.portfolio.name
-  role       = "roles/artifactregistry.writer"
+  role       = "roles/artifactregistry.admin"
   member     = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
-# resource "google_service_account_iam_member" "github_actions" {
-#   service_account_id = google_service_account.github_actions.name
-#   role               = "roles/iam.workloadIdentityUser"
-#   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/subject/konojunya/0xjj.dev"
-# }
-
-resource "google_service_account_iam_binding" "github_actions" {
+resource "google_service_account_iam_member" "github_actions" {
   service_account_id = google_service_account.github_actions.name
   role               = "roles/iam.workloadIdentityUser"
-  members            = ["principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/attribute.repository/konojunya/0xjj.dev"]
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/attribute.repository/konojunya/0xjj.dev"
 }
+
+# resource "google_service_account_iam_binding" "github_actions" {
+#   service_account_id = google_service_account.github_actions.name
+#   role               = "roles/iam.workloadIdentityUser"
+#   members            = ["principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/attribute.repository/konojunya/0xjj.dev"]
+# }
 
 ## Artifact Registry
 resource "google_artifact_registry_repository" "portfolio" {
