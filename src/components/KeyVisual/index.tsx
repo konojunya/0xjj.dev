0574cb2ce8f4ecd7214930aba6ef578f3ac0efdf
JJ <junya03dance@gmail.com>
2024-09-02 19:52:18 +0900

    feat: update keyvisual

--- src/components/KeyVisual/index.tsx
+++ src/components/KeyVisual/index.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/no-unknown-property */
 import { useEffect, useRef } from "react";
+import fontData from "@compai/font-fugaz-one/data/typefaces/normal-400.json";
 import { getProject, types } from "@theatre/core";
 import studio from "@theatre/studio";
 import * as THREE from "three";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
 import state from "./state.json";
 
 const project = getProject("Project JJ", { state });
@@ -31,73 +35,93 @@ camera.position.z = 50;
  */
 const scene = new THREE.Scene();
 
-/*
- * TorusKnot
+/**
+ * Text
  */
-const geometry = new THREE.TorusKnotGeometry(10, 3, 300, 16);
-const material = new THREE.MeshStandardMaterial({ color: "#f00" });
-material.color = new THREE.Color("#049ef4");
-material.roughness = 0.5;
+(() => {
+  const material = new THREE.MeshStandardMaterial({
+    color: "#ffffff",
+    emissive: "#e954f4",
+  });
 
-const mesh = new THREE.Mesh(geometry, material);
-mesh.castShadow = true;
-mesh.receiveShadow = true;
-scene.add(mesh);
+  const font = new FontLoader().parse(fontData as any);
 
-const torusKnotObject = sheet.object("TorusKnot", {
-  rotation: types.compound({
-    x: types.number(mesh.rotation.x, { range: [-2, 2] }),
-    y: types.number(mesh.rotation.y, { range: [-2, 2] }),
-    z: types.number(mesh.rotation.z, { range: [-2, 2] }),
-  }),
-});
+  const geometry = new TextGeometry("Hello, I'm JJ", {
+    font: font,
+    size: 6,
+    depth: 5,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 1,
+    bevelSize: 0.5,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  geometry.center(); // テキストを中心揃え
 
-torusKnotObject.onValuesChange((values) => {
-  const { x, y, z } = values.rotation;
+  // メッシュの作成と追加
+  const textGeometry2 = geometry.clone().deleteAttribute("normal");
+  const textGeometry3 = BufferGeometryUtils.mergeVertices(textGeometry2);
+  textGeometry3.computeVertexNormals();
 
-  mesh.rotation.set(x * Math.PI, y * Math.PI, z * Math.PI);
-});
+  const textMesh = new THREE.Mesh(textGeometry3, material);
+  textMesh.position.set(-1.5, 0, 0);
+  scene.add(textMesh);
 
-/*
+  const mainTextObject = sheet.object("MainText", {
+    position: types.compound({
+      x: types.number(textMesh.position.x, { range: [-20, 20] }),
+      y: types.number(textMesh.position.y, { range: [-20, 20] }),
+      z: types.number(textMesh.position.z, { range: [-20, 20] }),
+    }),
+    rotate: types.compound({
+      x: types.number(0, { range: [-2, 2] }),
+      y: types.number(0, { range: [-2, 2] }),
+      z: types.number(0, { range: [-2, 2] }),
+    }),
+  });
+
+  mainTextObject.onValuesChange((values) => {
+    const { position, rotate } = values;
+    textMesh.position.set(position.x, position.y, position.z);
+    textMesh.rotation.set(
+      rotate.x * Math.PI,
+      rotate.y * Math.PI,
+      rotate.z * Math.PI,
+    );
+  });
+})();
+
+/**
  * Lights
  */
+(() => {
+  // Ambient Light
+  const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
+  scene.add(ambientLight);
 
-// Ambient Light
-const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
-scene.add(ambientLight);
+  // Point light
+  const directionalLight = new THREE.DirectionalLight("#6c20ff", 90);
+  directionalLight.position.y = 20;
+  directionalLight.position.z = 20;
 
-// Point light
-const directionalLight = new THREE.DirectionalLight("#ff0000", 30 /* , 0, 1 */);
-directionalLight.position.y = 20;
-directionalLight.position.z = 20;
+  directionalLight.castShadow = true;
 
-directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 2048;
+  directionalLight.shadow.mapSize.height = 2048;
+  directionalLight.shadow.camera.far = 50;
+  directionalLight.shadow.camera.near = 1;
+  directionalLight.shadow.camera.top = 20;
+  directionalLight.shadow.camera.right = 20;
+  directionalLight.shadow.camera.bottom = -20;
+  directionalLight.shadow.camera.left = -20;
 
-directionalLight.shadow.mapSize.width = 2048;
-directionalLight.shadow.mapSize.height = 2048;
-directionalLight.shadow.camera.far = 50;
-directionalLight.shadow.camera.near = 1;
-directionalLight.shadow.camera.top = 20;
-directionalLight.shadow.camera.right = 20;
-directionalLight.shadow.camera.bottom = -20;
-directionalLight.shadow.camera.left = -20;
-
-scene.add(directionalLight);
-
-// RectAreaLight
-const rectAreaLight = new THREE.RectAreaLight("#ff0", 1, 50, 50);
-
-rectAreaLight.position.z = 10;
-rectAreaLight.position.y = -40;
-rectAreaLight.position.x = -20;
-rectAreaLight.lookAt(new THREE.Vector3(0, 0, 0));
-
-scene.add(rectAreaLight);
+  scene.add(directionalLight);
+})();
 
 /**
  * Renderer
  */
-
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.shadowMap.enabled = true;
