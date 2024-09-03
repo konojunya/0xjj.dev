42f4c4e0c83dd70b5c6aab1f62db99081a52ab02
JJ <junya03dance@gmail.com>
2024-09-03 09:49:18 +0900

    update: remove blur effect from top

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useEffect, useState } from "react";
-import { clsx } from "clsx";
 import { Works } from "src/components/Works";
-import { throttle } from "throttle-debounce";
-import styles from "./App.module.css";
 import { About } from "@/components/About";
 import { Contact } from "@/components/Contact";
 import { KeyVisual } from "@/components/KeyVisual";
 
 export const App: React.FC = () => {
-  const [blurPixel, setBlurPixel] = useState(0);
-
-  const handleScroll = throttle(100, () => {
-    const scroll = window.scrollY;
-    const documentHeight =
-      document.documentElement.scrollHeight - window.innerHeight;
-    const scrollProgress = scroll / documentHeight;
-    const progress = Math.floor(scrollProgress * 100);
-
-    setBlurPixel(Math.min(20, progress));
-  });
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
-
   return (
     <main className="relative left-0 top-0">
       <div className="fixed left-0 top-0 z-0">
@@ -36,13 +12,7 @@ export const App: React.FC = () => {
         <h1 className="sr-only">Hello, I'm JJ</h1>
       </div>
 
-      <div
-        className={clsx(
-          "relative left-0 top-0 z-10 mx-auto w-screen max-w-screen-2xl",
-          styles.blur,
-        )}
-        style={{ "--blur": `${blurPixel}px` } as React.CSSProperties}
-      >
+      <div className="relative left-0 top-0 z-10 mx-auto w-screen max-w-screen-2xl">
         {/* key visual 用の箱 */}
         <div className="h-screen" />
 
