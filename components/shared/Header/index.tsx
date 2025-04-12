e78644179e188c53ff648333db92cf5b4f365f55
JJ <junya03dance@gmail.com>
2025-04-13 04:56:36 +0900

    feat: AI ui

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -3,8 +3,9 @@
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import DecryptedText from "../bits/DecryptedText";
+import DecryptedText from "@/components/shared/bits/DecryptedText";
 import Link from "next/link";
+import { AI } from "./AI";
 
 export const Header: React.FC = () => {
   const { setTheme } = useTheme();
@@ -29,10 +30,13 @@ export const Header: React.FC = () => {
             />
           </h1>
         </Link>
-        <Button variant="outline" size="icon" onClick={handleTheme}>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
+        <div className="flex items-center gap-3">
+          <Button variant="outline" size="icon" onClick={handleTheme}>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+          <AI />
+        </div>
       </div>
     </header>
   );
