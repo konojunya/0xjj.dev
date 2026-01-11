7d018ecea86621501d7638c4b9cf976937197223
konojunya <junya03dance@gmail.com>
2026-01-11 17:51:06 +0900

    feat: init worker and vite react app

--- app/not-found.tsx
+++
@@ -1,5 +0,0 @@
-import { NotFound } from "@/components/pages/404";
-
-export default function page() {
-  return <NotFound />;
-}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Top from "@/components/pages/top";
-
-export default async function page() {
-  return <Top />;
-}
